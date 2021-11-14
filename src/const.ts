5fd63ba518ecbbabd77a76d59a0079751f8bf96a
zhou <maozhongzhou@moyi365.com>
2021-11-14 16:07:56 +0800

    feat: add function parameter refactor

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,11 @@
 import type * as ts from "typescript/lib/tsserverlibrary"
 import 'open-typescript'
+
+function matchReturnNode(typescript: typeof ts, token: ts.Node) {
+  if (typescript.isIdentifier(token))
+    return typescript.findAncestor(token, typescript.isFunctionDeclaration)
+}
+
 export const ACTIONS = {
   FunctionExpressionToArrowFunction: {
     match(typescript: typeof ts, token: ts.Node): token is { parent: ts.FunctionExpression } & ts.Node {
@@ -17,6 +23,16 @@ export const ACTIONS = {
     info: {
       name: 'Change Identifier name',
       description: 'Change Identifier name'
+    }
+  },
+  AddFunctionParameter: {
+    match(typescript: typeof ts, token: ts.Node): token is ts.Identifier {
+      return !!matchReturnNode(typescript, token)
+    },
+    matchReturnNode,
+    info: {
+      name: 'Add a parameter for the function',
+      description: 'Add a parameter for the function'
     }
   }
 }
